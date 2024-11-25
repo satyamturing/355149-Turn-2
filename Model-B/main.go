275f9349a44774d6ced88d70e28226ba7476dbab
satyamturing <satyam.d@turing.com>
2024-11-25 21:42:03 +0530

    Add files via upload

---
+++ Model-B/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+	"time"
+)
+
+const profileFile = "profile.mem"
+
+func startMemoryProfiling() {
+	f, err := os.Create(profileFile)
+	if err != nil {
+		log.Fatal("could not create profile file:", err)
+	}
+	pprof.WriteHeapProfile(f)
+	f.Close()
+	fmt.Println("Memory profile written to", profileFile)
+}
+
+func main() {
+	// ... existing code ...
+
+	startMemoryProfiling()
+
+	// ... code to be profiled ..
+
+	doSomeWork()
+
+	startMemoryProfiling()
+}
+
+func doSomeWork() {
+	// Simulate memory-intensive operations
+	var largeSlice = make([]int, 1_000_000)
+	for i := range largeSlice {
+		largeSlice[i] = i
+	}
+	time.Sleep(2 * time.Second)
+}
